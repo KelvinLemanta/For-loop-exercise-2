aec3e28322201d6dda5427ce7f054523b1ce140d
KelvinLemanta <85560626+KelvinLemanta@users.noreply.github.com>
2021-11-18 09:48:11 +0300

    Add files via upload

---
+++ for loop exercise 2.cpp
@@ -0,0 +1,21 @@
+
+//  137533 Kelvin Lemanta
+
+//  for loop to get sum of numbers between 20 to 25
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i=20;
+    int sum = 0;
+
+for (int i=20; i < 50; i++)
+{
+    sum = sum + i;
+    cout << sum << " " << i;
+}
+}
